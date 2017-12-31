25de517ec932d285a5742f5a098c9d8a23822856
zagorski32611 <j.j.zagorski@gmail.com>
2017-12-31 09:12:12 -0500

    that broke stuff. removed primary_key from model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  set_primary_key :employee_id
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
