5f3a8bec5f89a2cdb70e8014aa9ebcbe68a8d763
Joe Zagorski <j.j.zagorski@gmail.com>
2018-04-07 18:57:02 -0400

    creat ro functionality works

---
+++ app/assets/javascripts/create-ro.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  const hideRo = document.querySelector("#ro-create").style.display = "none";
+  toggleRO();
+  cancelRO();
+  
+}
+function toggleRO() {
+  document.querySelector("#create-ro-button").addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector("#ro-create").style.display = "block";
+  });
+  
+}
+
+function cancelRO() {
+  document.querySelector("#cancel-ro").addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector("#ro-create").style.display = "none";
+  });
+}
+
--- app/assets/javascripts/search-ro.js
+++ app/assets/javascripts/search-ro.js
@@ -1,24 +1,9 @@
 // window.onload = function() {
-//   newRo = document.getElementById("ro-create").style.display = 'none'
-//   this.document.getElementById("create-button").onClick=function() {
-    
-//     newRo.toggle()
+//     const hideSearch = document.querySelector("#ro-search").style.display = "none";
+  
+//     document.querySelector("#search-button").addEventListener("click", function(event) {
+//       event.preventDefault();
+//       document.querySelector("#ro-search").style.display = "block"
+//     })
 //   }
-// };
-
-// document.getElementById("ro-search").style.display = 'none'
-// document.querySelectorAll("ro-search-btn").forEach(function(item) {
-//   item.addEventListener("click", function(event) {
-//     event.preventDefault()
-//     document.querySelector("ro-search").id.display= 'block'
-//   })
-// })
-
-window.onload = function() {
-  const hideNewRo = document.querySelector("#ro-create").style.display = "none";
-
-  document.querySelector("#create-ro-button").addEventListener("click", function(event) {
-    event.preventDefault();
-    document.querySelector("#ro-create").style.display = 'block'
-  })
-}
+  
